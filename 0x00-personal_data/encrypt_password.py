23f0d59eccc11bebe5f75d36316f998b6a35c62b
MutabPato <patrickmtbz@gmail.com>
2024-08-04 11:33:08 -0700

    Encrypting passwords

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Personal data"""
+
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """Encrypting passwords"""
+    salt = bcrypt.gensalt()
+    hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed
