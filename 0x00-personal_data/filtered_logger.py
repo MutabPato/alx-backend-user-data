f588fe5ab8da88107b2ee7d5320f42e93f99dc3b
MutabPato <patrickmtbz@gmail.com>
2024-11-07 08:39:28 -0800

    Connect to secure database

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,6 +7,11 @@ Personal data
 from typing import List
 import re
 import logging
+import mysql.connector
+from os import environ
+
+
+PII_FIELDS = {"name", "email", "phone", "ssn", "password"}
 
 
 def filter_datum(
@@ -18,6 +23,36 @@ def filter_datum(
         message = re.sub(
                 f'{f}=.*?{separator}', f'{f}={redaction}{separator}', message)
     return message
+
+
+def get_logger() -> logging.Logger:
+    """Returns a logging.Logger object
+    """
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(RedactingFormatter(list(PII_FIELDS)))
+    logger.addHandler(stream_handler)
+
+    return logger
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """Returns a connector to the database"""
+    username = environ.get("PERSONAL_DATA_DB_USERNAME", "root")
+    password = environ.get("PERSONAL_DATA_DB_PASSWORD", "")
+    host = environ.get("PERSONAL_DATA_DB_HOST", "localhost")
+    db_name = environ.get("PERSONAL_DATA_DB_NAME")
+
+    conn = mysql.connector.connection.MySQLConnection(
+            user=username,
+            password=password,
+            host=host,
+            database=db_name)
+
+    return conn
 
 
 class RedactingFormatter(logging.Formatter):
