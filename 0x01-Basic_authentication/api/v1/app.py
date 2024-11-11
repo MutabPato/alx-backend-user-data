c5139c6671729b5c993108c1dbc31205e9b94fab
MutabPato <patrickmtbz@gmail.com>
2024-11-11 20:32:02 +0000

    Request validation!

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -7,11 +7,21 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
+from api.v1.auth.auth import Auth
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+
+# Initialize the auth variable
+
+auth = None
+auth = os.getenv("AUTH_TYPE")
+
+# Conditionally set up the authentication
+if auth:
+    auth = Auth()
 
 
 @app.errorhandler(404)
@@ -33,6 +43,22 @@ def forbidden_error(error) -> str:
     """ Forbidden handler
     """
     return jsonify({"error": "Forbidden"}), 403
+
+
+@app.before_request
+def before_request_handler():
+    """ Method to handler before_request
+    """
+    if auth is None:
+        return
+    path_list = [
+            '/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
+    if auth.require_auth(request.path, path_list) is False:
+        return
+    if auth.authorization_header(request) is None:
+        abort(401)
+    if auth.current_user(request) is None:
+        abort(403)
 
 
 if __name__ == "__main__":
