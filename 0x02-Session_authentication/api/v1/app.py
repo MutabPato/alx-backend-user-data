37193406be18e1473befa6a1ec878b7e61127075
MutabPato <patrickmtbz@gmail.com>
2024-11-14 20:13:26 +0000

    Before request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -59,7 +59,7 @@ def before_request_handler():
     if auth.require_auth(request.path, path_list) is False:
         return
     if auth.authorization_header(request) is None and auth.session_cookie(
-            request):
+            request) is None:
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
