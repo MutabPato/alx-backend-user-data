7384de34954c1dcf1e39e4aac3cc5e331626ff37
Patrick Mutabazi <patrickmtbz@gmail.com>
2024-11-19 19:03:20 +0300

    update user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,8 +5,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from sqlalchemy.exc import InvalidRequestError
-from sqlalchemy.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError, NoResultFound
 from user import Base, User
 
 
@@ -46,20 +45,27 @@ class DB:
     def find_user_by(self, **kwargs) -> User:
         """Find a user by arbitrary keyword arguments
         """
+        if not kwargs:
+            raise InvalidRequestError("No filter arguments provided.")
         try:
             user = self._session.query(User).filter_by(**kwargs).one()
             return user
         except NoResultFound:
             raise NoResultFound(f"No user found with filter {kwargs}")
-        except InvalidRequestError as e:
+        except Exception as e:
             raise InvalidRequestError(f"Invalid request: {e}")
 
     def update_user(self, user_id, **kwargs) -> None:
         """Updates a user
         """
-        user = self.find_user_by(id=user_id)
-        for key, value in kwargs.items():
-            if not hasattr(user, key):
-                raise ValueError(f"Invalid attribute: {key}")
-            setattr(user, key, value)
-        self._session.commit()
+        try:
+            user = self.find_user_by(id=user_id)
+            for key, value in kwargs.items():
+                if not hasattr(user, key):
+                    raise ValueError(f"Invalid attribute: {key}")
+                setattr(user, key, value)
+            self._session.commit()
+
+        except Exception as e:
+            self._session.rollback()
+            raise e
