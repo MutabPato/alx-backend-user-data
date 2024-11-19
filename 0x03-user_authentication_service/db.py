b945c95486752058930fb763112f29cc75e97d63
Patrick Mutabazi <patrickmtbz@gmail.com>
2024-11-19 18:44:09 +0300

    create user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -60,6 +60,6 @@ class DB:
         user = self.find_user_by(id=user_id)
         for key, value in kwargs.items():
             if not hasattr(user, key):
-                raise ValueError(f"Invalid attribute: {e}")
+                raise ValueError(f"Invalid attribute: {key}")
             setattr(user, key, value)
         self._session.commit()
