756cbf97f0306db67afb9e187597b68fb7e010f4
Patrick Mutabazi <patrickmtbz@gmail.com>
2024-11-20 01:08:23 +0300

    Get session ID

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -54,3 +54,15 @@ class Auth:
 
         except Exception:
             return False
+
+    def create_session(self, email: str) -> str:
+        """returns user's session ID as a string
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
+
+        except Exception:
+            return None
